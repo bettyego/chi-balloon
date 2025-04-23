c1aaac17bfb61365992706bd3e5b1dd2ffa97536
bettyego <nwabethroseonuoha@gmail.com>
2025-04-23 10:06:01 +0100

    fix a few errors

--- src/component/page/VideoPage.jsx
+++ src/component/page/VideoPage.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 const VideoPage = () => {
   return (
-    <div className="w-full h-screen">
-      <iframe
-        src="https://www.youtube.com/embed/U267pF2blpk"
-        title="Pinchofluxeevents Showcase Video"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        className="w-full h-full"
-      />
-    </div>
+    <section className="relative w-full h-screen overflow-hidden">
+      {/* Video Background with Sound */}
+      <video
+        autoPlay
+        loop
+        playsInline
+        controls // Optional: remove if you don’t want controls
+        className="absolute top-0 left-0 w-full h-screen object-cover z-0"
+      >
+        <source src="/chiVid.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </section>
   );
 };
 
