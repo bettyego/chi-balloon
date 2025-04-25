56fa9857dca274802cb56651bbc78c3f50e15038
bettyego <nwabethroseonuoha@gmail.com>
2025-04-25 15:46:45 +0100

    fix a few errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./component/page/Layout";
 import Home from "./component/Home";
-import AboutUs from "./component/page/Aboutus";
+import AboutUs from "./component/page/AboutUs"; // ✅ Capital "U"
 import Services from "./component/page/Services";
 import InquiryForm from "./component/page/InquiryForm";
 import ClientReview from "./component/page/ClientReview";
@@ -10,11 +10,10 @@ import ContactUs from "./component/page/ContactUs";
 
 function App() {
   return (
-    <> 
-
+    <>
       <Routes>
         <Route path="/" element={<Layout />}>
-        <Route path="/"  element={<Home />}/>
+          <Route path="/" element={<Home />} />
           <Route path="about" element={<AboutUs />} />
           <Route path="services" element={<Services />} />
           <Route path="inquiryform" element={<InquiryForm />} />
@@ -23,7 +22,6 @@ function App() {
           <Route path="contact" element={<ContactUs />} />
         </Route>
       </Routes>
-
     </>
   );
 }
